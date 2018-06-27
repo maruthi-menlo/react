aea5a02f95095d7998d29de3fd3d751887b87a0f
Maruthi <maruthir@menlo-technologies.com>
2018-06-27 19:00:30 +0530

    Code changes for login.

--- src/components/login/login.componet.jsx
+++ src/components/login/login.componet.jsx
@@ -40,22 +40,25 @@ class LoginComponent extends Component{
                         <p className="content-heading">Nam dapibus nisl vitae elit fringilla rutrum. Aenean sollicitudin, erat a elementum rutrum, neque sem pretium metus, quis mollis nisl nunc et massa.</p>
                         <p className="content-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam erat laoreet. Sed sit amet arcu aliquet, molestie justo at, auctor nunc. Phasellus ligula ipsum, volutpat eget semper id, viverra eget nibh. <br/>Suspendisse luctus mattis cursus. Nam consectetur ante at nisl hendrerit gravida.</p>
                     </Col>
-                    <Col lg={6} md={6} sm={6} xs={12} className="formContainer">
-                        <p className="labelSignIn">Already Have an account? Sign In</p>
-                        <Form horizontal id="loginForm">
-                            <FormGroup controlId="username">
-                                <ControlLabel className="labelFormControl">Username</ControlLabel>
-                                <FormControl type="text" placeholder="Username" className="formControl"/>
-                            </FormGroup>
-                            <FormGroup controlId="password">
-                                <ControlLabel className="labelFormControl">Password</ControlLabel>
-                                <FormControl type="password" placeholder="Password"  className="formControl"/>
-                            </FormGroup>
-                            <CBox id="rememberme" className="cbRemeberMe">
-                                &nbsp; Remember Username?
-                            </CBox>
-                            <Button className="btnText" onClick={this.loginFn}>Sign In</Button>
-                        </Form>
+                    <Col lg={6} md={6} sm={6} xs={12}>
+                        <div className="formContainer">
+                            <p className="labelSignIn">Already Have an account? Sign In</p>
+                            <Form horizontal id="loginForm">
+                                <FormGroup controlId="username">
+                                    <ControlLabel className="labelFormControl">Username</ControlLabel>
+                                    <FormControl type="text" placeholder="Username" className="formControl"/>
+                                </FormGroup>
+                                <FormGroup controlId="password">
+                                    <ControlLabel className="labelFormControl">Password</ControlLabel>
+                                    <FormControl type="password" placeholder="Password"  className="formControl"/>
+                                </FormGroup>
+                                <CBox id="rememberme" className="cbRemeberMe">
+                                    &nbsp; Remember Username?
+                                </CBox>
+                                <Button className="btnText" onClick={this.loginFn}>Sign In</Button>
+                                <div className="forgot-pwd"><a>Forgot Password?</a></div>
+                            </Form>
+                        </div>
                         <Loader isShow={this.state.showModal}></Loader>
                     </Col>
                 </Row>
