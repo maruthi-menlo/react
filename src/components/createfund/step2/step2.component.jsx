c145b6d16bcbe93c64b4e8fbb7a9f81380bd5a62
maruthi-menlo <maruthir@menlo-technologies.com>
2018-07-11 12:42:34 +0530

    step2 changes

--- src/components/createfund/step2/step2.component.jsx
+++ src/components/createfund/step2/step2.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../createfund.component.css';
 import { DropdownButton, Grid, Button, Checkbox as CBox, Row, Col, MenuItem, ControlLabel, FormControl, FormGroup } from 'react-bootstrap';
-
+import userDefaultImage from '../../../images/default_user.png';
 class Step2Component extends Component {
 
     constructor(props) {
@@ -17,32 +17,62 @@ class Step2Component extends Component {
                 <Button className="gpDelegateButton">Gp Delegate</Button>
                 <div className="checkBoxGpContainer">
                     <label className="Rectangle-6">
+                    <img src={userDefaultImage} alt="fund_image" className="gpdelegateImg" />
                         <span className="Ben-Parker">Ben Parker</span>
                         <CBox className="checkBoxBen">
                         </CBox>
                     </label>
                     <label className="Rectangle-6">
+                    <img src={userDefaultImage} alt="fund_image" className="gpdelegateImg" />
                         <span className="Ben-Parker">Jeff Lynne</span>
                         <CBox className="checkBoxBen">
                         </CBox>
                     </label>
                     <label className="Rectangle-6">
+                        <img src={userDefaultImage} alt="fund_image" className="gpdelegateImg" />
                         <span className="Ben-Parker">Kaitlyn Lopez</span>
                         <CBox className="checkBoxBen">
                         </CBox>
                     </label>
                     <label className="Rectangle-6">
+                        <img src={userDefaultImage} alt="fund_image" className="gpdelegateImg" />
                         <span className="Ben-Parker">Larry Croft</span>
                         <CBox className="checkBoxBen">
                         </CBox>
                     </label>
                     <label className="Rectangle-6">
+                        <img src={userDefaultImage} alt="fund_image" className="gpdelegateImg" />
                         <span className="Ben-Parker">Samrutha Karujika</span>
                         <CBox className="checkBoxBen">
                         </CBox>
                     </label>
                     <label className="Rectangle-6">
-                        <span className="Ben-Parker">Samrutha Karujika</span>
+                        <img src={userDefaultImage} alt="fund_image" className="gpdelegateImg" />
+                        <span className="Ben-Parker">Sarmad Ahallah</span>
+                        <CBox className="checkBoxBen">
+                        </CBox>
+                    </label>
+                    <label className="Rectangle-6">
+                        <img src={userDefaultImage} alt="fund_image" className="gpdelegateImg" />
+                        <span className="Ben-Parker">Terrence Osborne</span>
+                        <CBox className="checkBoxBen">
+                        </CBox>
+                    </label>
+                    <label className="Rectangle-6">
+                        <img src={userDefaultImage} alt="fund_image" className="gpdelegateImg" />
+                        <span className="Ben-Parker">Virginia Tang</span>
+                        <CBox className="checkBoxBen">
+                        </CBox>
+                    </label>
+                    <label className="Rectangle-6">
+                        <img src={userDefaultImage} alt="fund_image" className="gpdelegateImg" />
+                        <span className="Ben-Parker">Xiao Pang</span>
+                        <CBox className="checkBoxBen">
+                        </CBox>
+                    </label>
+                    <label className="Rectangle-6">
+                        <img src={userDefaultImage} alt="fund_image" className="gpdelegateImg" />
+                        <span className="Ben-Parker">Zita Hoffenheimer</span>
                         <CBox className="checkBoxBen">
                         </CBox>
                     </label>
